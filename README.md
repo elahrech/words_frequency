ed62ce2360106d684a34b60d01a7f273d500841e
Elmehdi <143545038+elahr23@users.noreply.github.com>
2025-05-07 11:32:44 +0200

    test: add unit tests for words_frequency

---
+++ README.md
@@ -0,0 +1,4 @@
+# Words Frequency Project
+
+## Description
+This project calculates the frequency of words in a given sentence and returns the top `n` most frequent words. This is a technical assessment for Foodles.
---
+++ tests/test_words_frequency.py
@@ -0,0 +1,50 @@
+from src.words_frequency import words_frequency
+
+
+def test_words_frequency_example():
+    sentence = "bar baz foo foo zblah zblah zblah baz toto bar"
+    n = 3
+    expected_output = [("zblah", 3), ("bar", 2), ("baz", 2)]
+    assert words_frequency(sentence, n) == expected_output
+
+
+def test_words_frequency_tie_breaker():
+    sentence = "bar baz foo foo zblah zblah"
+    n = 2
+    expected_output = [("foo", 2), ("zblah", 2)]
+    assert words_frequency(sentence, n) == expected_output
+
+
+def test_words_frequency_single_word():
+    sentence = "foo"
+    n = 1
+    expected_output = [("foo", 1)]
+    assert words_frequency(sentence, n) == expected_output
+
+
+def test_words_frequency_empty_sentence():
+    sentence = ""
+    n = 3
+    expected_output = []
+    assert words_frequency(sentence, n) == expected_output
+
+
+def test_words_frequency_more_n_than_words():
+    sentence = "baz foo zblah"
+    n = 5
+    expected_output = [("baz", 1), ("foo", 1), ("zblah", 1)]
+    assert words_frequency(sentence, n) == expected_output
+
+
+def test_words_frequency_with_punctuation():
+    sentence = "foo, bar! baz? foo..."
+    n = 2
+    expected_output = [("foo", 2), ("bar", 1)]
+    assert words_frequency(sentence, n) == expected_output
+
+
+def test_words_frequency_case_sensitivity():
+    sentence = "Foo foo FOO bar Bar BaR"
+    n = 2
+    expected_output = [("bar", 3), ("foo", 3)]
+    assert words_frequency(sentence, n) == expected_output
